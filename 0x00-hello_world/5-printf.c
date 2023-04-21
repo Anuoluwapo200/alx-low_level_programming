71cd0d8bed130bf97a710a0a3da5820190cc4ad2
Anuoluwapo200 <anuoluwapoagbede00@gmail.com>
2023-04-20 17:21:38 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints "with proper grammer, but the outcome is
- * a piece of art," followed by a  new line.
- * Return: Always 0.
- *
+ * main - Entry point
+ * 
+ * Return: Always 0(Success)
  */
 
 int main(void)
