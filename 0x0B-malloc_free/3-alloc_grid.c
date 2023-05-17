70539c6c898d1b696fba54640f42191aa3b5b98a
Anuoluwapo200 <anuoluwapoagbede00@gmail.com>
2023-05-17 04:18:17 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -32,7 +32,7 @@ int **alloc_grid(int width, int height)
 
 			for (; x >= 0; x--)
 				free(mee[x]);
-		
+
 			free(mee);
 			return (NULL);
 		}
@@ -44,7 +44,6 @@ int **alloc_grid(int width, int height)
 		for (y = 0; y < width; y++)
 			mee[x][y] = 0;
 	}
-	
 	return (mee);
 }
 
