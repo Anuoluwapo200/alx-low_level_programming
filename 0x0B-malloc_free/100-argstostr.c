8ffb219124b31eb69ea1e850a9ca2078d40ad236
Anuoluwapo200 <anuoluwapoagbede00@gmail.com>
2023-05-17 04:56:16 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,19 +13,14 @@ char *argstostr(int ac, char **av)
 
 {
 	int i, n, r = 0, l = 0;
-
 	char *str;
 
 	if (ac == 0 || av == NULL)
-
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
-
 	{
-
 		for (n = 0; av[i][n]; n++)
-
 			l++;
 	}
 
@@ -38,19 +33,12 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
-
 	{
-
 	for (n = 0; av[i][n]; n++)
-
 	{
-
 		str[r] = av[i][n];
-
 		r++;
-
 	}
-
 	if (str[r] == '\0')
 	{
 		str[r++] = '\n';
